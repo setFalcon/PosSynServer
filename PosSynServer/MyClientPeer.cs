b26f5fbff11fb810632b3b3811ecc2b904e7d9f5
setFalcon <1449530193@qq.com>
2020-03-03 16:53:28 +0800

    #008-接收服务端发送的事件和数据

--- PosSynServer/MyClientPeer.cs
+++ PosSynServer/MyClientPeer.cs
@@ -9,24 +9,29 @@ namespace PosSynServer {
 
         //处理客户端请求
         protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters) {
-            byte opCode = operationRequest.OperationCode;//区分请求类型
+            byte opCode = operationRequest.OperationCode; //区分请求类型
             switch (opCode) {
-                case 1: 
+                case 1:
                     PosSynServer.Log.Info("收到客户端请求");
                     //接受Parameters
-                    Dictionary<byte,object> data = operationRequest.Parameters;
+                    Dictionary<byte, object> data = operationRequest.Parameters;
                     object intValue;
                     data.TryGetValue(1, out intValue);
                     object stringValue;
                     data.TryGetValue(2, out stringValue);
-                    PosSynServer.Log.Info(intValue + " " + stringValue);//打印信息
+                    PosSynServer.Log.Info(intValue + " " + stringValue); //打印信息
                     //响应
                     OperationResponse resp = new OperationResponse();
                     resp.OperationCode = 1;
                     Dictionary<byte, object> dataResp = new Dictionary<byte, object>();
-                    dataResp.Add(1,"Hello Client");
+                    dataResp.Add(1, "Hello Client");
                     resp.SetParameters(dataResp);
-                    SendOperationResponse(resp,sendParameters);//响应客户端的请求
+                    SendOperationResponse(resp, sendParameters); //响应客户端的请求
+
+                    EventData eventData = new EventData();
+                    eventData.Code = 1;
+                    eventData.SetParameters(dataResp);
+                    SendEvent(eventData, sendParameters); //服务器发送事件
                     break;
                 default: break;
             }
