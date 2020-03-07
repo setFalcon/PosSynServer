992c4b854beabc529ee94fb5af6030ffba63d0a8
setFalcon <1449530193@qq.com>
2020-03-07 15:13:06 +0800

    #023-添加位置同步时间码以及位置存储结构

--- ConnectBridge/OperationCode.cs
+++ ConnectBridge/OperationCode.cs
@@ -3,5 +3,6 @@ namespace ConnectBridge {
         Default,
         Login,
         Register,
+        SyncPos,
     }
 }
---
+++ ConnectBridge/Vector3Data.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ConnectBridge {
+    [Serializable]
+    public class Vector3Data {
+        public float x, y, z;
+    }
+}
